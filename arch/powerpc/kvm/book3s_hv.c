6cb8a3e07ae170628818d28302c5de2abb0970b4
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2012-12-15 23:44:38 +0530

    powerpc: Decode the pte-lp-encoding bits correctly.
    We look at both the segment base page size and actual page size and store
    the pte-lp-encodings in an array per base page size.
    
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- arch/powerpc/kvm/book3s_hv.c
+++ arch/powerpc/kvm/book3s_hv.c
@@ -1515,7 +1515,12 @@ static void kvmppc_add_seg_page_size(struct kvm_ppc_one_seg_page_size **sps,
 	(*sps)->page_shift = def->shift;
 	(*sps)->slb_enc = def->sllp;
 	(*sps)->enc[0].page_shift = def->shift;
-	(*sps)->enc[0].pte_enc = def->penc;
+	/*
+	 * FIXME!!
+	 * This is returned to user space. Do we need to
+	 * return details of MPSS here ?
+	 */
+	(*sps)->enc[0].pte_enc = def->penc[linux_psize];
 	(*sps)++;
 }
 
