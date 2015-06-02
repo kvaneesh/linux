50c453499886ca593c9735258653dc5d801757ad
Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>
2015-06-02 14:07:32 +0530

    powerpc/mm: Move hash page table related functions to pgtable-hash64.c
    Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>

--- arch/powerpc/include/asm/book3s/64/hash.h
+++ arch/powerpc/include/asm/book3s/64/hash.h
@@ -527,6 +527,8 @@ static inline void hpte_do_hugepage_flush(struct mm_struct *mm,
 }
 #endif /* CONFIG_TRANSPARENT_HUGEPAGE */
 
+extern int map_kernel_page(unsigned long ea, unsigned long pa,
+			   unsigned long flags);
 #endif /* !__ASSEMBLY__ */
 #endif /* __KERNEL__ */
 #endif /* _ASM_POWERPC_BOOK3S_64_HASH_H */
